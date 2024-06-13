1f124191177ec801bbae792c9ae42e5396c28d2f
chjm44 <chc807000@naver.com>
2024-06-13 20:21:02 +0900

    모르겠다

--- Performance evaluation app/Form1.Designer.cs
+++ Performance evaluation app/Form1.Designer.cs
@@ -42,14 +42,14 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(174, 104);
+            this.textBox1.Location = new System.Drawing.Point(174, 160);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 21);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(174, 158);
+            this.textBox2.Location = new System.Drawing.Point(174, 106);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 21);
             this.textBox2.TabIndex = 1;
@@ -58,7 +58,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("굴림", 10F);
-            this.label1.Location = new System.Drawing.Point(103, 111);
+            this.label1.Location = new System.Drawing.Point(103, 167);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(50, 14);
             this.label1.TabIndex = 2;
@@ -68,7 +68,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("굴림", 10F);
-            this.label2.Location = new System.Drawing.Point(103, 160);
+            this.label2.Location = new System.Drawing.Point(103, 108);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(50, 14);
             this.label2.TabIndex = 3;
@@ -89,19 +89,20 @@
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(41, 214);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(63, 12);
+            this.label3.Size = new System.Drawing.Size(87, 12);
             this.label3.TabIndex = 5;
-            this.label3.Text = "객관식 1 : ";
+            this.label3.Text = "객관식 1 : 오답";
             this.label3.Visible = false;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(41, 242);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(63, 12);
+            this.label4.Size = new System.Drawing.Size(87, 12);
             this.label4.TabIndex = 6;
-            this.label4.Text = "객관식 1 : ";
+            this.label4.Text = "객관식 2 : 오답";
             this.label4.Visible = false;
             // 
             // label5
@@ -109,9 +110,9 @@
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(41, 268);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(63, 12);
+            this.label5.Size = new System.Drawing.Size(87, 12);
             this.label5.TabIndex = 7;
-            this.label5.Text = "객관식 1 : ";
+            this.label5.Text = "객관식 3 : 오답";
             this.label5.Visible = false;
             // 
             // label6
@@ -119,9 +120,9 @@
             this.label6.AutoSize = true;
             this.label6.Location = new System.Drawing.Point(259, 225);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(63, 12);
+            this.label6.Size = new System.Drawing.Size(87, 12);
             this.label6.TabIndex = 8;
-            this.label6.Text = "객관식 1 : ";
+            this.label6.Text = "주관식 1 : 오답";
             this.label6.Visible = false;
             // 
             // label7
@@ -129,9 +130,9 @@
             this.label7.AutoSize = true;
             this.label7.Location = new System.Drawing.Point(259, 258);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(63, 12);
+            this.label7.Size = new System.Drawing.Size(87, 12);
             this.label7.TabIndex = 9;
-            this.label7.Text = "객관식 1 : ";
+            this.label7.Text = "주관식 2 : 오답";
             this.label7.Visible = false;
             // 
             // Form1
@@ -151,6 +152,7 @@
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- Performance evaluation app/Form3.cs
+++ Performance evaluation app/Form3.cs
@@ -12,6 +12,8 @@ namespace Performance_evaluation_app
 {
     public partial class Form3 : Form
     {
+        public static bool score1;
+        public static bool score2;
         public Form3()
         {
             InitializeComponent();
@@ -27,6 +29,16 @@ namespace Performance_evaluation_app
 
         private void button1_Click(object sender, EventArgs e)
         {
+
+            if (textBox1.Text == "정답")
+            {
+                score1 = true;
+            }
+            if (textBox2.Text == "정답")
+            {
+                score2 = true;
+            }
+
             Form1 form1 = new Form1();
             form1.Show(); 
             this.Hide();
